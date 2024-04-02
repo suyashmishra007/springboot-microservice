e4d398270b2ab1ad408342a00637788a920e8508
Suyash Mishra <suyyash25@gmail.com>
2024-04-02 18:17:37 +0530

    implement webclient using webflux

--- employee-service/src/main/java/net/microservices/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/microservices/employeeservice/EmployeeServiceApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
-  @Bean// We just need to create a method,that returns an instance of the class and we need to annotate that method with @Bean configuration
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
+//  @Bean// We just need to create a method,that returns an instance of the class and we need to annotate that method with @Bean configuration
+//  public RestTemplate restTemplate() {
+//    return new RestTemplate();
+//  }
+  @Bean
+  public WebClient webClient(){
+    return WebClient.builder().build();
   }
   public static void main(String[] args) {
     SpringApplication.run(EmployeeServiceApplication.class, args);
--- employee-service/src/main/java/net/microservices/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/microservices/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -12,13 +12,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
   private EmployeeRepository employeeRepository;
-  private RestTemplate restTemplate;
+//  private RestTemplate restTemplate;
+  private WebClient webClient;
   @Override
   public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
     Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
@@ -29,11 +31,10 @@ public class EmployeeServiceImpl implements EmployeeService {
   @Override
   public APIResponseDto getEmployeeById(Long employeeId) {
     Employee employee = employeeRepository.findById(employeeId).get();
-    System.out.println(employee.toString());
-    ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("https://localhost:8080/api/departments/"+employee.getDepartmentCode(),DepartmentDto.class);
-    DepartmentDto departmentDto = responseEntity.getBody();
+//    ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(),DepartmentDto.class);
+    DepartmentDto departmentDto = webClient.get().uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode()).retrieve().bodyToMono(DepartmentDto.class).block();
+//    DepartmentDto departmentDto = responseEntity.getBody();
     EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
-
     APIResponseDto apiResponseDto = new APIResponseDto();
     apiResponseDto.setEmployee(employeeDto);
     apiResponseDto.setDepartment(departmentDto);
